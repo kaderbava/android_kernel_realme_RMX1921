dakkshesh07/parallax_kernel_realme_RMX1921

oppo_touchscreen: touchpanel_common_driver: Fix up logging and silence
Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -50,7 +50,7 @@
 #define TPD_DEBUG(a, arg...)\
     do{\
         if (LEVEL_DEBUG == tp_debug)\
-            pr_err("[TP]"TPD_DEVICE ": " a, ##arg);\
+            pr_debug("[TP]"TPD_DEVICE ": " a, ##arg);\
     }while(0)
 
 #define TPD_DETAIL(a, arg...)\
@@ -5534,7 +5534,7 @@ int register_common_touch_device(struct touchpanel_data *pdata)
     ts->msm_drm_notif.notifier_call = msm_drm_notifier_callback;
     ret = msm_drm_register_client(&ts->msm_drm_notif);
     if (ret)
-        TPD_ERR("Unable to register msm_drm_notifier: %d\n", ret);
+        TPD_INFO("Unable to register msm_drm_notifier: %d\n", ret);
 #endif
 
     //step15 : workqueue create(speedup_resume)

kaderbava/android_kernel_realme_RMX1921

drivers/input/touchscreen/oppo_touchscreen/touchpanel_common_driver.c
@@ -49,13 +49,13 @@
 #define TPD_DEBUG(a, arg...)\
     do{\
         if (LEVEL_DEBUG == tp_debug)\
-            pr_err("[TP]"TPD_DEVICE_COMMON ": " a, ##arg);\
+            pr_debug("[TP]"TPD_DEVICE_COMMON ": " a, ##arg);\
     }while(0)
 
 #define TPD_DETAIL(a, arg...)\
     do{\
         if (LEVEL_BASIC != tp_debug)\
-            pr_err("[TP]"TPD_DEVICE_COMMON ": " a, ##arg);\
+            pr_debug("[TP]"TPD_DEVICE_COMMON ": " a, ##arg);\
     }while(0)
 
 #define TPD_SPECIFIC_PRINT(count, a, arg...)\
@@ -5533,7 +5533,7 @@ int register_common_touch_device(struct touchpanel_data *pdata)
     ts->msm_drm_notif.notifier_call = msm_drm_notifier_callback;
     ret = msm_drm_register_client(&ts->msm_drm_notif);
     if (ret)
-        TPD_ERR("Unable to register msm_drm_notifier: %d\n", ret);
+        TPD_INFO("Unable to register msm_drm_notifier: %d\n", ret);
 #endif
 
     //step15 : workqueue create(speedup_resume)