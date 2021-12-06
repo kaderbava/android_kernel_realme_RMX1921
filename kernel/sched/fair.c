5b07c911e58f2f092e324712128ea0078c43b6d6
Dakkshesh <dakkshesh5@gmail.com>
2021-12-06 13:20:18 +0000

    sched: fair: fix warning

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6270,9 +6270,9 @@ static inline bool task_fits_max(struct task_struct *p, int cpu)
 	if (capacity == max_capacity)
 		return true;
 
-	if (task_boost_policy(p) == SCHED_BOOST_ON_BIG &&
-			is_min_capacity_cpu(cpu) ||
-		uclamp_boosted(p) > 10)
+	if ((task_boost_policy(p) == SCHED_BOOST_ON_BIG ||
+			uclamp_boosted(p) > 10) &&
+			is_min_capacity_cpu(cpu))
 		return false;
 
 	return __task_fits(p, cpu, 0);
