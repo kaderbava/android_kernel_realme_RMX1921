d77cd7e01cba290ab97b44ff0c97debddad95a09
ThatisJigen <gianfranco.liguori.96@gmail.com>
2022-02-16 13:46:26 +0100

    drivers: backlight: scale brightness to our panel * our panel max is 1023 but on some dtb is reported 2047, even overriding the values in dts we got some issues on scaling and I don't like that solution. Cause this kernel is used only by us let's make a check based on max brightness and handle both cases

--- drivers/video/backlight/backlight.c
+++ drivers/video/backlight/backlight.c
@@ -177,13 +177,25 @@ int backlight_device_set_brightness(struct backlight_device *bd,
 
 	mutex_lock(&bd->ops_lock);
 	if (bd->ops) {
-		if (brightness > bd->props.max_brightness)
-			rc = -EINVAL;
-		else {
-			pr_debug("set brightness to %lu\n", brightness);
-			bd->props.brightness = brightness;
-			rc = backlight_update_status(bd);
-		}
+        if (bd->props.max_brightness < 2047){
+            if (brightness > bd->props.max_brightness)
+                rc = -EINVAL;
+            else {
+                pr_debug("set brightness to %lu\n", brightness);
+                bd->props.brightness = brightness;
+                rc = backlight_update_status(bd);
+            }
+        } else {
+            if ((brightness / 2) > 1023)
+                bd->props.brightness = 1023;
+            else {
+            if ((brightness / 2) < 1)
+                bd->props.brightness = 1;
+            else
+                bd->props.brightness = brightness / 2;
+            }
+            rc = backlight_update_status(bd);
+        }
 	}
 	mutex_unlock(&bd->ops_lock);
 
